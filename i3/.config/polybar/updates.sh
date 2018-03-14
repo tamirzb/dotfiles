40dbaf7969c1a01d1fa8c1598b32fd970556f3fc
Tamir <tamir.z3@gmail.com>
2018-03-14 16:36:47 +0200

    i3: Add AUR updates to polybar

--- i3/.config/polybar/updates.sh
+++ i3/.config/polybar/updates.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-check=$(checkupdates | wc -l)
+updates_arch=$(checkupdates | wc -l)
+updates_aur=$(cower -u 2>/dev/null | wc -l)
+updates=$(("$updates_arch" + "$updates_aur"))
 
-if [[ "$check" != "0" ]]
-then
-    echo "$check %{F#5b5b5b}%{F-}"
+if [ "$updates" -gt 0 ]; then
+    echo "$updates_arch %{F#5b5b5b}%{F-} $updates_aur"
 fi
