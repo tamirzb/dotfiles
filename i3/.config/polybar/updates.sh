06d4adb503409f1380beb12cd8d09d93b542de92
Tamir <tamir.z3@gmail.com>
2019-04-03 20:39:14 +0300

    i3: Show AUR updates using pikaur instead of cower.

--- i3/.config/polybar/updates.sh
+++ i3/.config/polybar/updates.sh
@@ -2,7 +2,7 @@
 
 export CHECKUPDATES_DB="/tmp/checkup-db-$MONITOR"
 updates_arch=$(checkupdates | wc -l)
-updates_aur=$(cower -u 2>/dev/null | wc -l)
+updates_aur=$(pikaur -Qua 2> /dev/null | wc -l)
 updates=$(("$updates_arch" + "$updates_aur"))
 
 if [ "$updates" -gt 0 ]; then
