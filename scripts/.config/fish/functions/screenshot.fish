92c24582b74e0986a53c0a41c4fbbffc98465b95
Tamir <tamir.z3@gmail.com>
2019-05-06 01:57:30 +0200

    scripts: Use maim for screenshots instead of imagemagick
    screenshot script is now maim, but with the result put into both
    clipboard and /tmp/screen.png

--- scripts/.config/fish/functions/screenshot.fish
+++ scripts/.config/fish/functions/screenshot.fish
@@ -1,3 +1,3 @@
-function screenshot
-    import png:- | xclip -selection clipboard -t image/png -i
+function screenshot -w maim
+    maim $argv | tee /tmp/screen.png | xclip -selection clipboard -t image/png -i
 end
