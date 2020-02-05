4fdd5d9349e033051d3908652a3505fb3af32958
Tamir <tamir.z3@gmail.com>
2020-02-04 22:10:23 -0600

    scripts: Fix wpaconnect to actually connect
    Instead of just printing the command needed to connect

--- scripts/.config/fish/functions/wpaconnect.fish
+++ scripts/.config/fish/functions/wpaconnect.fish
@@ -1,4 +1,4 @@
 function wpaconnect -a ssid
-    read -s -P "WPA Passphrase: " -l pass
-    echo sudo wpa_supplicant -c (wpa_passphrase $ssid $pass | psub) $argv[2..-1]
+    read -P "WPA Passphrase: " -l pass
+    sudo wpa_supplicant -c (wpa_passphrase $ssid $pass | psub) $argv[2..-1]
 end
