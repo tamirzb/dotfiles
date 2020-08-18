c738884195dfc709920be662fdbe59cd0a0f4387
Tamir <tamir.z3@gmail.com>
2020-08-19 01:55:01 +0300

    scripts: Don't remove current file in snapshots-rm
    This should keep the existing file and only remove it in the snapshots
    in order to prevent accidents. The existing file can be removed
    separately.

--- scripts/.config/fish/functions/snapshots-rm.fish
+++ scripts/.config/fish/functions/snapshots-rm.fish
@@ -2,9 +2,6 @@
 function snapshots-rm -a subvol_path file_path
     set -l rm_params $argv[3..-1]
 
-    echo "Removing $subvol_path/$file_path"
-    rm $rm_params $subvol_path/$file_path
-
     for snapshot_num in (ls -1 $subvol_path/.snapshots)
         set -l snapshot $subvol_path/.snapshots/$snapshot_num/snapshot
         btrfs property set $snapshot ro false
