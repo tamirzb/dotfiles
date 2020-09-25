22aa9c5fbbd944c57844305dabe41955c05c6fbc
Tamir <tamir.z3@gmail.com>
2020-09-25 04:00:08 +0300

    sway: Make it easier to re-exec sway
    All the sway setup is now in a fish function, which can be easily
    re-executed

--- sway/.config/fish/conf.d/sway.fish
+++ sway/.config/fish/conf.d/sway.fish
@@ -1,10 +1,4 @@
 # Autostart sway on tty1
 if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-    # Execute Firefox in Wayland
-    set -x MOZ_ENABLE_WAYLAND 1
-    # Execute some QT apps (e.g. Wireshark) in Wayland
-    set -x QT_QPA_PLATFORM wayland
-
-    set -x XDG_SESSION_TYPE wayland
-    systemd-cat --stderr-priority=warning sway
+    sway
 end
---
+++ sway/.config/fish/functions/sway.fish
@@ -0,0 +1,9 @@
+function sway
+    # Execute Firefox in Wayland
+    set -x MOZ_ENABLE_WAYLAND 1
+    # Execute some QT apps (e.g. Wireshark) in Wayland
+    set -x QT_QPA_PLATFORM wayland
+
+    set -x XDG_SESSION_TYPE wayland
+    systemd-cat --stderr-priority=warning sway $argv
+end
