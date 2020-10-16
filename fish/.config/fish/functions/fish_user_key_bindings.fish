2a3d4b9ab143b0d892a8aec8079ff9fab4bd8439
Tamir <tamir.z3@gmail.com>
2020-10-17 02:35:39 +0300

    fish: Add FZF key bindings

---
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,7 @@
+function fish_user_key_bindings
+    # FZF key bindings:
+    # Ctrl+T: Select files/folders (can select multiple with Tab)
+    # Ctrl+R: Search in command history
+    # Alt+C: Change directory
+    fzf_key_bindings
+end
