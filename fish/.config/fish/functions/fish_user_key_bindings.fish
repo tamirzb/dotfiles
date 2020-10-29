91263a307079b0b52f8dc071922c1d7bb66f19a5
Tamir <tamir.z3@gmail.com>
2020-10-29 20:42:01 +0200

    fish: Add an FZF "cd backwards" binding

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -4,6 +4,13 @@ function fish_user_key_bindings
     # Ctrl+R: Search in command history
     # Alt+C: Change directory
     fzf_key_bindings
+    # Alt+Shift+C: Change directory backwards. Taken from fzf's wiki.
+    function fzf-bcd-widget -d 'cd backwards'
+        pwd | awk -v RS=/ '/\n/ {exit} {p=p $0 "/"; print p}' | tac | eval (__fzfcmd) +m --select-1 --exit-0 $FZF_BCD_OPTS | read -l result
+        [ "$result" ]; and cd $result
+        commandline -f repaint
+    end
+    bind \eC "fzf-bcd-widget"
 
     # Use Ctrl+X to disown a job
     bind \cx "disown 2>/dev/null && commandline -f force-repaint"
