2bb8af3a27238e5a176fc79daf00255a40234da5
Tamir <tamir.z3@gmail.com>
2023-06-25 20:24:29 +0800

    fzf: Add some bindings for the preview window

--- fzf/.config/fish/conf.d/fzf.fish
+++ fzf/.config/fish/conf.d/fzf.fish
@@ -3,7 +3,13 @@
 set -l bindings \
     "ctrl-d:half-page-down" \
     "ctrl-u:half-page-up" \
-    "ctrl-g:top"
+    "ctrl-g:top" \
+    # Scroll the preview window with Alt+u/d
+    "alt-u:preview-half-page-up" \
+    "alt-d:preview-half-page-down" \
+    # Ctrl+r to toggle preview between being on the right, being bottom or
+    # being hidden
+    "ctrl-r:change-preview-window\(bottom\|hidden\|right\)"
 
 set -x FZF_DEFAULT_OPTS \
     # Reverse lists results top to bottom with search bar on top
