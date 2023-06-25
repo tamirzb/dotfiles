22226c0aedaeef1bbcbc734fbd4d3c9e6c79ec40
Tamir <tamir.z3@gmail.com>
2023-06-25 20:09:11 +0800

    fzf: Change default layout
    Make the default layout more like the FZF binding for Ctrl+r, intended
    to be part of the shell (by only taking 50%) not take over it completely
    (by taking the whole window).

--- fzf/.config/fish/conf.d/fzf.fish
+++ fzf/.config/fish/conf.d/fzf.fish
@@ -1,6 +1,13 @@
+# Set the default options for FZF
+
 set -l bindings \
     "ctrl-d:half-page-down" \
     "ctrl-u:half-page-up" \
     "ctrl-g:top"
 
-set -x FZF_DEFAULT_OPTS "--bind="(string join "," $bindings)
+set -x FZF_DEFAULT_OPTS \
+    # Reverse lists results top to bottom with search bar on top
+    "--layout=reverse" \
+    # Take 50% of the window so it's still possible to see previous commands
+    "--height=50%" \
+    "--bind="(string join "," $bindings)
