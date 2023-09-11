b4344afab931d8045f44adbc89c9b3df6feaff9b
Tamir <tamir.z3@gmail.com>
2023-09-11 16:02:16 +0800

    fish: Make sudo wrapper handle fish functions

--- fish/.config/fish/functions/sudo.fish
+++ fish/.config/fish/functions/sudo.fish
@@ -1,3 +1,11 @@
 function sudo -w sudo
+    # Handle fish functions: If argument is a fish function then spawn another
+    # fish shell in sudo to run it.
+    # From: https://bsago.me/tech-notes/sudo-with-aliases-in-fish
+    if functions -q -- $argv[1]
+        set -l new_args (string join ' ' -- (string escape -- $argv))
+        set argv fish -c "$new_args"
+    end
+
     command sudo -E $argv
 end
