f5a9ec84fde7c07100f3c933ae1356b2b1761d60
Tamir <tamir.z3@gmail.com>
2024-04-01 20:39:53 +0800

    fish: Use Alt+C to copy current command line
    Apparently the default was Ctrl+X, which was overwritten

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,9 @@
 function fish_user_key_bindings
     # Use Ctrl+X to disown a job
     bind \cx "disown 2>/dev/null && commandline -f force-repaint"
+
+    # Switch copying of current command line to Alt+C (default was Ctrl+X)
+    # (Default for Alt+C is to capitalize a word but it doesn't seem very
+    #  useful)
+    bind \ec fish_clipboard_copy
 end
