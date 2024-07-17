907e79a0ab39058cd9e192dfbac56a9739717655
Tamir <tamir.z3@gmail.com>
2024-07-17 17:08:22 +0300

    scripts: Update myip to also support IPv6
    OpenDNS seemed to not return a result if you have an IPv6, so instead
    use a similar server provided by Google that can handle both cases.

--- scripts/.config/fish/functions/myip.fish
+++ scripts/.config/fish/functions/myip.fish
@@ -1,3 +1,9 @@
 function myip
-    dig myip.opendns.com @resolver1.opendns.com +short
+    set -l args "-4"
+    if test "$argv[1]" = "6"
+        set args "-6"
+    end
+    set -l ip (dig $args TXT o-o.myaddr.l.google.com @ns1.google.com +short)
+    # Remove the parentheses that Google adds
+    echo $ip | string sub -s 2 -e -1
 end
