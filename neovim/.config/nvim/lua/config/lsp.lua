e8251270b5d1c0617b8bc24d3ceb72c7e91cfb28
Tamir <tamir.z3@gmail.com>
2025-04-03 19:43:32 +0200

    neovim: Use LspAttach event to run on LSP attach
    Instead of adding the callback to every LSP configuration. This is a bit
    cleaner and can now move the keymaps to later in the file where it makes
    a little bit more sense.

--- neovim/.config/nvim/lua/config/lsp.lua
+++ neovim/.config/nvim/lua/config/lsp.lua
@@ -5,84 +5,7 @@ local fzf_lua = require('fzf-lua')
 local colors = require("config.colors")
 local utils = require("config.utils")
 
--- Set LSP related settings only after a language server is attached
-local on_attach = function(client, bufnr)
-    -- Keymaps are sort of based on the README of nvim-lspconfig
-
-    -- Keymaps in normal mode
-    utils.which_key_register({
-        -- Like ctags Ctrl+], but with <leader>
-        -- TODO: Possibly change this to Ctrl+] (and maybe put Ctrl+] in
-        --       another keymap) once I feel this can more or less replace
-        --       ctags
-        ["<leader>]"] = { vim.lsp.buf.definition, "Go to definition" },
-        -- Show a floating window with information about current symbol
-        ["<leader>h"] = { vim.lsp.buf.hover, "Symbol info" },
-
-        -- Show the diagnostic message for the current line (in case it's too
-        -- long to be displayed fully)
-        ["<leader>d"] = { vim.diagnostic.open_float, "Diagnostics info" },
-        -- Remap the default diganoistics navigation keymaps to not wrap
-        ["[d"] = { function() vim.diagnostic.jump({ count = -1 }) end,
-                   "Prev diagnostic" },
-        ["]d"] = { function() vim.diagnostic.jump({ count = 1 }) end,
-                   "Next diagnostic" },
-        -- Navigate only between error diagnostic messages
-        ["[D"] = {
-            function() vim.diagnostic.jump({
-                count = -1, severity = vim.diagnostic.severity.ERROR
-            }) end,
-            "Prev error diagnostic"
-        },
-        ["]D"] = {
-            function() vim.diagnostic.jump({
-                count = 1, severity = vim.diagnostic.severity.ERROR
-            }) end,
-            "Next error diagnostic"
-        },
-
-        -- FZF keymaps
-        -- Note that the tags keybindings are overwriting the existing ones
-        -- that don't use LSP
-        ["<leader>t"] = {
-            fzf_lua.lsp_document_symbols,
-            "Fuzzy search current buffer symbols"
-        },
-        ["<leader>T"] = {
-            fzf_lua.lsp_live_workspace_symbols,
-            "Fuzzy search project symbols"
-        },
-        ["<leader>l"] = { fzf_lua.lsp_references, "Fuzzy search references" }
-    }, { buffer = bufnr })
-
-    -- Keymaps in insert mode
-    utils.which_key_register({
-        -- Use the omnifunc for Ctrl+N
-        ["<c-n>"] = { function ()
-            -- This hack is needed so when we're in the middle of a "normal"
-            -- Ctrl+N (i.e. performed via Ctrl+X,Ctrl+N) and we press Ctrl+N,
-            -- it will continue with the normal one and not use the omnifunc
-            if vim.fn.pumvisible() == 1 then
-                vim.api.nvim_input("<c-x><c-n>")
-            else
-                vim.lsp.omnifunc(1, 0)
-            end
-        end, "LSP omnifunc complete" },
-
-        -- Use the "regular" Ctrl+N with Ctrl+X,Ctrl+N
-        ["<c-x><c-n>"] = { "<c-n>", "Regular Ctrl+N" }
-    }, {mode = "i", buffer = bufnr })
-end
-
--- Remove neovim default LSP mapping of K to hover, already using <leader>h
-vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(ev)
-        vim.keymap.del("n", "K", { buffer = ev.buf })
-    end,
-})
-
 M.clangd = {
-    on_attach = on_attach,
     init_options = {
         clangdFileStatus = true
     },
@@ -96,7 +19,6 @@ M.clangd = {
 }
 
 M.lua_ls = {
-    on_attach = on_attach,
     settings = { Lua = {
         runtime = { version = 'LuaJIT' },
         workspace = {
@@ -146,6 +68,76 @@ vim.diagnostic.config({
         wrap = false
     }
 })
+
+-- Set LSP related settings only after a language server is attached
+vim.api.nvim_create_autocmd("LspAttach", { callback = function(args)
+    -- Remove neovim default LSP mapping of K to hover, already using <leader>h
+    vim.keymap.del("n", "K", { buffer = args.buf })
+
+    -- Keymaps in normal mode
+    utils.which_key_register({
+        -- Like ctags Ctrl+], but with <leader>
+        -- TODO: Possibly change this to Ctrl+] (and maybe put Ctrl+] in
+        --       another keymap) once I feel this can more or less replace
+        --       ctags
+        ["<leader>]"] = { vim.lsp.buf.definition, "Go to definition" },
+        -- Show a floating window with information about current symbol
+        ["<leader>h"] = { vim.lsp.buf.hover, "Symbol info" },
+
+        -- Show the diagnostic message for the current line (in case it's too
+        -- long to be displayed fully)
+        ["<leader>d"] = { vim.diagnostic.open_float, "Diagnostics info" },
+        -- Remap the default diganoistics navigation keymaps to not wrap
+        ["[d"] = { function() vim.diagnostic.jump({ count = -1 }) end,
+                   "Prev diagnostic" },
+        ["]d"] = { function() vim.diagnostic.jump({ count = 1 }) end,
+                   "Next diagnostic" },
+        -- Navigate only between error diagnostic messages
+        ["[D"] = {
+            function() vim.diagnostic.jump({
+                count = -1, severity = vim.diagnostic.severity.ERROR
+            }) end,
+            "Prev error diagnostic"
+        },
+        ["]D"] = {
+            function() vim.diagnostic.jump({
+                count = 1, severity = vim.diagnostic.severity.ERROR
+            }) end,
+            "Next error diagnostic"
+        },
+
+        -- FZF keymaps
+        -- Note that the tags keybindings are overwriting the existing ones
+        -- that don't use LSP
+        ["<leader>t"] = {
+            fzf_lua.lsp_document_symbols,
+            "Fuzzy search current buffer symbols"
+        },
+        ["<leader>T"] = {
+            fzf_lua.lsp_live_workspace_symbols,
+            "Fuzzy search project symbols"
+        },
+        ["<leader>l"] = { fzf_lua.lsp_references, "Fuzzy search references" }
+    }, { buffer = args.buf })
+
+    -- Keymaps in insert mode
+    utils.which_key_register({
+        -- Use the omnifunc for Ctrl+N
+        ["<c-n>"] = { function ()
+            -- This hack is needed so when we're in the middle of a "normal"
+            -- Ctrl+N (i.e. performed via Ctrl+X,Ctrl+N) and we press Ctrl+N,
+            -- it will continue with the normal one and not use the omnifunc
+            if vim.fn.pumvisible() == 1 then
+                vim.api.nvim_input("<c-x><c-n>")
+            else
+                vim.lsp.omnifunc(1, 0)
+            end
+        end, "LSP omnifunc complete" },
+
+        -- Use the "regular" Ctrl+N with Ctrl+X,Ctrl+N
+        ["<c-x><c-n>"] = { "<c-n>", "Regular Ctrl+N" }
+    }, {mode = "i", buffer = args.buf })
+end, })
 
 -- Set LSP colors
 require("config.utils").apply_highlights({
