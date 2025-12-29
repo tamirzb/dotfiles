b5587d415fde1bc356ef213967991d7ca8ac7174
Tamir <tamir.z3@gmail.com>
2025-12-29 15:47:56 +0100

    fish: Add colors configuration
    This was in the fish universal variables but fish 4.3 changed to avoid
    this (which makes sense tbh). So now also include all the fish colors in
    this repository.

---
+++ fish/.config/fish/conf.d/colors.fish
@@ -0,0 +1,29 @@
+set --global fish_color_autosuggestion 555 brblack
+set --global fish_color_cancel -r
+set --global fish_color_command --bold
+set --global fish_color_comment red
+set --global fish_color_cwd green
+set --global fish_color_cwd_root red
+set --global fish_color_end brmagenta
+set --global fish_color_error brred
+set --global fish_color_escape bryellow --bold
+set --global fish_color_history_current --bold
+set --global fish_color_host normal
+set --global fish_color_host_remote yellow
+set --global fish_color_jobs red
+set --global fish_color_match --background=brblue
+set --global fish_color_normal normal
+set --global fish_color_operator bryellow
+set --global fish_color_param cyan
+set --global fish_color_quote yellow
+set --global fish_color_redirection brblue
+set --global fish_color_search_match white --background=brblack
+set --global fish_color_selection white --bold --background=brblack
+set --global fish_color_status red
+set --global fish_color_user brgreen
+set --global fish_color_valid_path --underline
+set --global fish_pager_color_completion
+set --global fish_pager_color_description B3A06D yellow
+set --global fish_pager_color_prefix white --bold --underline
+set --global fish_pager_color_progress brwhite --background=cyan
+set --global fish_pager_color_selected_background -r
