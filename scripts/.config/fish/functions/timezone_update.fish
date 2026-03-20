3b447d59c92f739342511edd1b800370839cef1a
Tamir <tamir.z3@gmail.com>
2026-03-20 20:33:36 +0800

    scripts: Change timezone_update to a different API provider
    The old one would return rate limited errors quite often

--- scripts/.config/fish/functions/timezone_update.fish
+++ scripts/.config/fish/functions/timezone_update.fish
@@ -2,5 +2,5 @@
 # Based on
 # https://wiki.archlinux.org/title/System_time#Update_timezone_every_time_NetworkManager_connects_to_a_network
 function timezone_update
-    sudo timedatectl set-timezone (curl --fail https://ipapi.co/timezone)
+    sudo timedatectl set-timezone (curl -s http://ip-api.com/json?fields=timezone | jq -r '.timezone')
 end
